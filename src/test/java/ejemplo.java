eecec19bb6d9ff68b209e40104450e08d9d4d3c9
Kevin Crispin Gonzales <kcrispin@emeal.nttdata.com>
2022-06-16 23:21:38 -0500

    actualizar place agregado

--- src/test/java/ejemplo.java
+++ src/test/java/ejemplo.java
@@ -1,5 +1,10 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ResponseBodyExtractionOptions;
 // se tiene que importar el static manualmente
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -11,35 +16,38 @@ public class ejemplo {
 //        WHEN -  es donde golpeo al metodo
 //        THEN -  donde hago las validaciones
 
-
-
+        String place_id = "";
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given().log().all()
-                .queryParam("key","qaclick123")
+        String response = given().log().all()
+                .queryParam("key", "qaclick123")
                 .header("Content-Type", "application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}")
+                .body(ApiUtil.bodyAddPlaceApi())
                 .when().log().all()
                 .post("maps/api/place/add/json")
                 .then().log().all()
                 .assertThat()
                 .statusCode(200)
 //                validar el un valor de la respuesta
-                .body("scope", equalTo("APP"));
+                .body("scope", equalTo("APP"))
+                .extract().response().asString();
+
+        JsonPath jsonPath = new JsonPath(response);
+        place_id = jsonPath.getString("place_id");
+
+
+        //ACTUALIZANDO EL LUGAR
+
+        System.out.println("==============ACTUALIZANDO PLACE================");
+
+        String responsePut = given()
+                .queryParam("key", "qaclick123")
+                .body(ApiUtil.bodyPutPlaceApi(place_id))
+                .when().put("maps/api/place/update/json")
+                .then().log().all()
+                .assertThat().statusCode(200)
+                .extract().response().asString();
+
+
     }
 }
